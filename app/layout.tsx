8ef040b0917eea0470cf7e9ceecafad9e71500b3
fernando benegas <benegasfernando1993@gmail.com>
2026-03-02 23:44:30 -0300

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,11 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      {children}
+      <SpeedInsights />
       </body>
     </html>
   );
